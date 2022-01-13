9dd49ef339d6a5cefd3632606f5dda791c4b043a
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-13 11:39:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,28 @@ resource "aws_s3_bucket" "aws-b1" {
     Environment = "Dev"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["574159413545"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ADO_AWS_TF_EC2"
+  }
+}
