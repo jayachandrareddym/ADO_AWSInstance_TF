632b01d672d2fb4d8ce13d2d66af855082ab7a9a
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-13 11:37:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    description = "The region where to provision resources"
+    type = string
+}
+variable "bucket_name" {
+    description = "The name of S3 bucket"
+    default = "terraform-bucket"
+    type = string
+}
+
+variable "bucket_name" {
+    description = "The name of S3 bucket"
+    default = "terraform-bucket"
+    type = string
+}
